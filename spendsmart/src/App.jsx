f4c7dd2a5e1700b70b618e2522395bc1361f2e57
mchaudhry02 <mchaudhry20@hotmail.com>
2026-03-08 14:54:13 -0500

    Core expense tracking functionality

--- spendsmart/src/App.jsx
+++ spendsmart/src/App.jsx
@@ -4,6 +4,11 @@ function App() {
       <h1>Hello LaunchCode</h1>
     </div>
   )
+
+  const [expenses, setExpenses] = useState([
+  { id: 1, title: "Food", amount: 15 },
+  { id: 2, title: "Gas", amount: 40 }
+])
 }
 
 export default App
